90303588869ffc21c30b79423c030773f5b094ff
natalia.orlova <natalia.orlova@messaggio.com>
2023-06-20 17:20:42 +0300

    fix

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
-    mongoose
-    .connect(url, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true})
-};
+    return (
+    mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
+)};
 
 
 module.exports = connectDB;
