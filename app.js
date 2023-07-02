9cffd3ab15e4af8bdbfba0d4df515b2fc416a57b
natalia.orlova <natalia.orlova@messaggio.com>
2023-07-02 13:12:16 +0300

    feat: added all methods

--- app.js
+++ app.js
@@ -8,13 +8,11 @@ const { connect } = require('mongoose');
 require('dotenv').config()
 
 //middleware
+app.use(express.static('./public'))
 app.use(express.json());
 
 
 //routes
-app.get('/hello', (res, req) => {
-    res.send('Hello backend')
-})
 
 app.use('/api/v1/tasks', tasks);
 
